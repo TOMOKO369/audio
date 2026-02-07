b2317b1c75fb5a9eb227792ce2ec1c93ece374c5
TOMOKO369 <78290454+TOMOKO369@users.noreply.github.com>
2026-02-08 07:19:43 +0900

    表示されない

---
+++ backend/start_server_debug.bat
@@ -0,0 +1,16 @@
+@echo off
+cd /d "%~dp0"
+if not exist venv (
+    echo Creating virtual environment...
+    python -m venv venv
+    call venv\Scripts\activate
+    pip install -r requirements.txt
+) else (
+    call venv\Scripts\activate
+)
+
+echo Checking dependencies...
+pip install -r requirements.txt
+
+echo Starting Backend Server...
+python main.py
